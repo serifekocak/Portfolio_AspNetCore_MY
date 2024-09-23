2baccc648a3dae29a4270efa4092af83e2517cc7
serifekocak <sserifekocakk@gmail.com>
2024-09-24 00:08:49 +0300

    24.09.2024

---
+++ Business/Concrete/SocialMediaManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class SocialMediaManager : ISocialMediaService
+    {
+        ISocialMediaDal _socialMedyaDal;
+
+        public SocialMediaManager(ISocialMediaDal socialMedyaDal)
+        {
+            _socialMedyaDal = socialMedyaDal;
+        }
+
+        public void TAdd(SocialMedia t)
+        {
+            _socialMedyaDal.Insert(t);
+        }
+
+        public void TDelete(SocialMedia t)
+        {
+            _socialMedyaDal.Delete(t);
+        }
+
+        public SocialMedia TGetByID(int id)
+        {
+            return _socialMedyaDal.GetByID(id);
+        }
+
+        public List<SocialMedia> TGetList()
+        {
+            return _socialMedyaDal.GetList();
+        }
+
+        public void TUpdate(SocialMedia t)
+        {
+            _socialMedyaDal.Update(t);
+        }
+    }
+}
---
+++ Portfolio_AspNetCore_MY/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_AspNetCore_MY.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();   
+        }
+    }
+}
