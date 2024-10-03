a77b700822e4ac39b239cd2995ea64a07d22de7b
serifekocak <sserifekocakk@gmail.com>
2024-10-03 12:23:24 +0300

    3.10.2024

---
+++ Portfolio_AspNetCore_MY/ViewComponents/About/AboutList.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_AspNetCore_MY.ViewComponents.About
+{
+    public class AboutList : ViewComponent
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal()); 
+        public IViewComponentResult Invoke()
+        {
+            var values = aboutManager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ Portfolio_AspNetCore_MY/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_AspNetCore_MY.ViewComponents.Feature
+{
+    public class FeatureList: ViewComponent 
+    {
+        //FeatureManager veri erişim sınıfını (EfFeatureDal) kullanarak iş kurallarını uygular
+        // Entity Framework sınıfının kullanılma sebebi generic ve özel metotları birden içinde barındırmasıdır.
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    }
+}
